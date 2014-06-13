44563ebf2b05a1998990743640f2a1924c7acb75
Marielle Volz <marielle.volz@gmail.com>
2014-06-13 15:08:59 +0100

    Add server file to route external queries
    1. Takes POST data of one param (URL) at localhost/ve
    2. Sends query to Zotero instance
    3. Receives Zotero response body
    4. Sends Zotero body in response
    
    Currently contains some unused code (commented out).
    
    Change-Id: If7b4fd7990dd1207d2a0ce0e247a49b0d926c347

---
+++ server.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+/**
+ * https://www.mediawiki.org/wiki/cite-from-id
+ */
+
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var zotero_request = require('./zotero.js').zotero_request;
+
+var port = '1970';
+
+var zotero_url = 'http://localhost:1969/web' //assumes zotero already started
+
+/*testing below*/
+//test_url = "http://www.tandfonline.com/doi/abs/10.1080/15424060903167229"
+test_sessionid = "abc123"
+
+/*methods*/
+//var convert_response = function(service_name, callback){
+	//if (service_name == 'zotero'){
+		//callback(body);
+	//}
+	//else {
+		//console.log('No translators currently exist for that service');
+	//}
+
+//}
+
+//CiteFromID (CFID) service
+var cfid = express();
+
+//SECURITY WARNING: FOR TESTING PURPOSES, ALLOWS ALL REQUEST ORIGINS
+cfid.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
+// parse application/json
+//cfid.use(bodyParser.json())
+
+/*URL for VE requests*/
+cfid.post('/ve', function(req, res){
+
+	//Retrieve query params from request
+	var requested_url = req.body.url;
+
+	//send response
+
+	zotero_request(requested_url, test_sessionid, function(body){
+		res.type('application/json');
+		res.json(body);
+	});
+});
+
+cfid.listen(port);
+
+console.log('Server running on http://localhost:'+port);
+
+/*Exports*/
+exports = module.exports = cfid;

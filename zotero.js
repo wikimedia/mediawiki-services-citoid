8f5dd990c519590e1d8c9584c1b466a643dc9ba7
Marielle Volz <marielle.volz@gmail.com>
2014-06-16 13:46:04 +0100

    ZoteroRequest now flattens creator fields
    Added .json file with sample Zotero response for faster testing.
    
    Added modifyBody method to zotero.js to alter the response.
    
    Currently removes creator field and replaces it with flattened
    fields that citation templates could take, such as editor1-first for
    the first name of an editor. Note that there is currently no
    citation template that takes author1-last directly; equivalent fields
    on en.Wikipedia, for example, might be last1, author1, last, etc.
    Because each wiki's citation templates are different,
    fields would have to be assigned to their equivalent field in
    TemplateData to be used.
    
    Change-Id: I17b161dad696a9f6e4346b7fc89736c9d6aeae84

--- zotero.js
+++ zotero.js
@@ -21,23 +21,60 @@ var zoteroRequest  = function(requestedURL, sessionID, callback){
 
 	request(options, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
-			callback(body);
+			callback(modifyBody(body));
 		}
 	});
 };
 
-/*Test URL in main function*/
-var main = function(){
+/*Currently replaces creators obj list with flat set of fields*/
+var modifyBody = function(body){
+	var creatorTypeCount = {};
+	var zotCreators = body[0].creators;
+
+	for (z in zotCreators){
+		creatorFieldName = zotCreators[z].creatorType;
+		if (creatorTypeCount[creatorFieldName]){
+			creatorTypeCount[creatorFieldName] += 1;
+		}
+		else {
+			creatorTypeCount[creatorFieldName] = 1;
+		}
+		//Appends number to name, i.e. author -> author1
+		creatorFieldName += (parseInt(creatorTypeCount[creatorFieldName])); 
+
+		body[0][creatorFieldName + "-first"] = zotCreators[z].firstName;
+		body[0][creatorFieldName + "-last"] = zotCreators[z].lastName;
+	}
+
+	delete body[0].creators; //remove creators field
+	//body[0] += newCreators; //append new creator fields to end of body
+	//console.log(body);
+	return body;
+}
+
+/*Test server fcns*/
+var testServer = function(){
     testURL = "http://www.tandfonline.com/doi/abs/10.1080/15424060903167229"
 	testSessionID = "abc123"
 
 	zoteroRequest(testURL, testSessionID, function(body){
-		console.log(body);
+		console.log(modifyBody(body));
 	});
 }
 
+/*Test response alterations without having to use server*/
+var testJSON = function(){
+	var sampleJSON = require("./sampleZoteroResponseBody.json");
+	console.log("before:");
+	console.log(JSON.stringify(sampleJSON));
+	console.log("after:");
+	console.log(JSON.stringify(modifyBody(sampleJSON)));
+}
+
+/*Test methods in main */
 if (require.main === module) {
-    main();
+	//testJSON();
+	testServer();
 }
 
 /*Exports*/

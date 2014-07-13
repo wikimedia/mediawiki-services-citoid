c825ff60a9664dcc5692f67c86c690d46abe95f2
Marielle Volz <marielle.volz@gmail.com>
2014-07-13 15:35:36 +0100

    Bug fix; wrapped html parser in try block
    Failure was occuring when html parser threw
    errors.
    
    Wrapped parseFromString in a try catch block.
    
    Change-Id: I0f930e2fbfd857f44262f2ab485e1595f2cf4cb1

--- scrape.js
+++ scrape.js
@@ -7,8 +7,14 @@ var xpath = require('xpath');
 var dom = require('xmldom').DOMParser;
 var request = require('request');
 
-//Currently picks out contents of <title> tag only
-//returns list of json obj (i.e. body)
+
+/**
+ * Currently picks out contents of <title> tag only
+ * callback runs on list of json objs (var body)
+ * @param  {String}   url      url to scrape
+ * @param  {Function} callback callback function
+ */
+
 var scrapeXpath = function(url, callback){
 	var json = { title : "", url: url};
 	request(
@@ -17,23 +23,34 @@ var scrapeXpath = function(url, callback){
 			headers: {'user-agent': 'Mozilla/5.0'},
 			//followRedirect: false 
 		}, function(url, response, html){
-			var doc = new dom().parseFromString(html);
-			var titleValue = '';
+			var doc, 
+				titleValue = '';
+
+			try{
+				doc = new dom().parseFromString(html);
+			}
+			catch (e){
+				console.log(e);
+			}
+
 			try {
 				titleValue = xpath.select("//title/text()", doc).toString();
 			}
-			catch (e){}
+			catch (e){
+				console.log(e);
+			}
+
 			json.title = titleValue;
 			var body = [json];
 			callback(body);
 	});
-}
+};
 
 var scrape = scrapeXpath;
 
 if (require.main === module) {
-	var sampleUrl = 'http://www.mediawiki.org';
-	console.log('scrape fcn running on sample url:'+sampleUrl)
+	var sampleUrl = 'http://example.com';
+	console.log('scrape fcn running on sample url:'+sampleUrl);
 	scrape(sampleUrl, function(body){
 		console.log(body);
 	});

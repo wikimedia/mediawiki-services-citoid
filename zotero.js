0ba083f45efa6560e69fcb917e40d5338b177e0d
Marielle Volz <marielle.volz@gmail.com>
2014-08-02 12:03:32 +0100

    Improved citoid response from zotero responses
        More fields added to zotero-requested citoid res
    
        *Added url field where zotero doesn't return one
        *Added pmid field from zotero's "extra" fields
    
        Fixes to make more robust to Zotero errors
    
        *Catch an additional 500 error
        *Deals with differing data structure from some
        translators (i.e. extra wrapping array)
        *Logging requests to console
    
    Change-Id: I0cc9785bd6d97b89e1414306b3e87c1f71ebce7c

--- zotero.js
+++ zotero.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 /**
- * https://www.mediawiki.org/wiki/cite-from-id
+ * https://www.mediawiki.org/wiki/citoid
 * 
 * Supplies methods to send requests to a Zotero server
  */
 
 var request = require('request');
-//var Promise = require('bluebird');
-var async = require('async');
 
 var zoteroRequest  = function(zoteroURL, requestedURL, sessionID, callback){
 	var options = {
@@ -21,8 +19,8 @@ var zoteroRequest  = function(zoteroURL, requestedURL, sessionID, callback){
 
 	request(options, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
-			//modify body if response is okay
-			callback(error, response, modifyBody(body));
+			//modify body only if response is okay
+			callback(error, response, modifyBody(requestedURL, body));
 		}
 		else {
 			callback(error, response, body);
@@ -31,37 +29,71 @@ var zoteroRequest  = function(zoteroURL, requestedURL, sessionID, callback){
 	});
 };
 
-/*Currently replaces creators obj list with flat set of fields*/
-var modifyBody = function(body){
-	var creatorTypeCount = {};
-	var zotCreators = body[0].creators;
+/*Converts Zotero body into Citoid body*/
+var modifyBody = function(url, body){
+	var citation, zotCreators,
+		creatorTypeCount = {};
 
-	for (z in zotCreators){
-		creatorFieldName = zotCreators[z].creatorType;
-		if (creatorTypeCount[creatorFieldName]){
-			creatorTypeCount[creatorFieldName] += 1;
-		}
-		else {
-			creatorTypeCount[creatorFieldName] = 1;
-		}
-		//Appends number to name, i.e. author -> author1
-		creatorFieldName += (parseInt(creatorTypeCount[creatorFieldName])); 
-
-		body[0][creatorFieldName + "-first"] = zotCreators[z].firstName;
-		body[0][creatorFieldName + "-last"] = zotCreators[z].lastName;
+	//hack
+	//in most cases body will be an arrat, but in some will be an array of arrays
+	if (!(body[0] instanceof Array)){
+		citation = body[0];
+	}
+	else if (!(body[0][0] instanceof Array)){
+		citation = body[0][0];
+	}
+	else {
+		return body;
 	}
 
-	delete body[0].creators; //remove creators field
-	return body;
-}
+	if (citation.creators) {
+		zotCreators = citation.creators;
+
+		for (var z in zotCreators){
+			creatorFieldName = zotCreators[z].creatorType;
+			if (creatorTypeCount[creatorFieldName]){
+				creatorTypeCount[creatorFieldName] += 1;
+			}
+			else {
+				creatorTypeCount[creatorFieldName] = 1;
+			}
+			//Appends number to name, i.e. author -> author1
+			creatorFieldName += (parseInt(creatorTypeCount[creatorFieldName])); 
+
+			citation[creatorFieldName + "-first"] = zotCreators[z].firstName;
+			citation[creatorFieldName + "-last"] = zotCreators[z].lastName;
+		}
+
+		//delete citation.creators; //remove creators field
+	}
+
+	//some zotero requests come back without the url field filled in
+	if (!citation.url){
+		citation.url = url;
+	}
+
+	//get pmid out of extra fields
+	if (citation.extra){
+		var extraFields = citation.extra.split('\n');
+		for (var f in extraFields){
+			//could add them all, but let's not do this in case of conflicting fields
+			var keyValue = extraFields[f].split(': ');
+			if (keyValue[0] === 'PMID'){
+				citation[keyValue[0]] = keyValue[1].trim();
+			}
+		}
+	}
+
+	//console.log(citation);
+	return [citation];
+};
 
 /*Test server fcns*/
 var testServer = function(){
-	var zoteroURL = 'http://localhost:1969/web'; //assumes zotero already started
-
-    var testURL = "http://www.tandfonline.com/doi/abs/10.1080/15424060903167229"; //URL that works with Zotero
-	//testURL = "http://books.google.co.uk/books?hl=en&lr=&id=7lueAgAAQBAJ&oi=fnd&pg=PR5&dq=mediawiki&ots=-Z0o2LCgao&sig=IGHnyWEiNiNvPyXeyCuOcdvi15s#v=onepage&q=mediawiki&f=false" //url that doesn't work with zotero
-	var testSessionID = "abc123";
+	var zoteroURL = 'http://localhost:1969/web', //edit this to your own endpoint
+    	testURL = "http://www.tandfonline.com/doi/abs/10.1080/15424060903167229", //URL that works with Zotero
+		//testURL = "http://books.google.co.uk/books?hl=en&lr=&id=7lueAgAAQBAJ&oi=fnd&pg=PR5&dq=mediawiki&ots=-Z0o2LCgao&sig=IGHnyWEiNiNvPyXeyCuOcdvi15s#v=onepage&q=mediawiki&f=false", //url that doesn't work with zotero
+		testSessionID = "abc123";
 
 	zoteroRequest(zoteroURL, testURL, testSessionID, function(error, response, body){
 		if (response) {
@@ -72,9 +104,9 @@ var testServer = function(){
 				console.log(body);
 			}
 		}
-		else {console.log("Server at "+zoteroURL+" does not appear to be running.")}
+		else {console.log("Server at "+zoteroURL+" does not appear to be running.");}
 	});
-}
+};
 
 /*Test response alterations without having to use server*/
 var testJSON = function(){
@@ -83,7 +115,7 @@ var testJSON = function(){
 	console.log(JSON.stringify(sampleJSON));
 	console.log("after:");
 	console.log(JSON.stringify(modifyBody(sampleJSON)));
-}
+};
 
 /*Test methods in main */
 if (require.main === module) {

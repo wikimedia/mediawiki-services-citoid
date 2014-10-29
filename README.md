aacf7b84f0871786ccbb026ee9d1047ae219f204
Marielle Volz <marielle.volz@gmail.com>
2014-10-29 12:44:03 +0000

    Adds GET endpoint /api, takes URL & DOI
    * New /api endpoint
    	* Takes GET requests
    	* Cache enabled (client, not server-side)
    	* Takes both URL and DOI
    * Moved files to /lib/
    	* unshorten.js now follows all redirects
    	* distingush.js determines whether search
    	  term is DOI, or URL as default
    	* Most request logic moved to request.js
    * All responses now have request errors
            * Previously if a request error, body
              was sent with URL as title
            * Now body is still sent, but with 520
              error.
    * 'mediawiki' format in /lib/zotero.js changed
            * ISBN now list of isbns
    	* ISSN now list of issns
    * README.md created
    
    Change-Id: Ia4b58d4b9eda3871c239c50bbacdce814bb3c134

---
+++ README.md
@@ -0,0 +1,5 @@
+#Citoid
+
+Citoid is a node.js service which currently locates citation data given a URL.
+
+[Citoid Documentation on mediawiki.org](https://www.mediawiki.org/wiki/Citoid)
--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 /**
  * https://www.mediawiki.org/wiki/citoid
  */
+
 /*external modules*/
-var express = require('express');
-var request = require('request');
 var bodyParser = require('body-parser');
+var bunyan = require('bunyan');
+var express = require('express');
+var path = require('path');
 var urlParse = require('url');
 var util = require('util');
-var path = require('path');
-var unshorten = require('./unshorten.js');
 var opts = require('yargs')
 	.usage('Usage: $0 [-c configfile|--config=configfile]')
 	.default({
@@ -18,9 +18,8 @@ var opts = require('yargs')
 	.alias( 'c', 'config' );
 var argv = opts.argv;
 
-/*internal modules*/	
-var zoteroRequest = require('./zotero.js').zoteroRequest;
-var scrape = require('./scrape.js').scrape;
+var distinguish = require('./lib/distinguish.js').distinguish;
+var requestFromURL = require('./lib/requests.js').requestFromURL;
 
 /* import local settings*/
 var settingsFile = path.resolve(process.cwd(), argv.c);
@@ -29,18 +28,15 @@ var citoidPort = CitoidConfig.citoidPort;
 var citoidInterface = CitoidConfig.citoidInterface;
 var zoteroPort = CitoidConfig.zoteroPort;
 var zoteroInterface = CitoidConfig.zoteroInterface;
-var wskey = CitoidConfig.wskey;
 var debug = CitoidConfig.debug;
 var allowCORS = CitoidConfig.allowCORS;
 
 //url base which allows further formatting by adding a single endpoint, i.e. 'web'
-var zoteroURL = util.format('http://%s:%s/%s', zoteroInterface, zoteroPort.toString()); 
-
-/*testing variables*/
-var testSessionID = "abc123";
+var zoteroURL = util.format('http://%s:%s/%s', zoteroInterface, zoteroPort.toString());
 
 //Init citoid webserver
 var citoid = express();
+var log = bunyan.createLogger({name: "citoid"});
 
 //SECURITY WARNING: ALLOWS ALL REQUEST ORIGINS
 //change allowCORS in localsettings.js
@@ -52,8 +48,11 @@ citoid.all('*', function(req, res, next) {
 
 citoid.use(bodyParser.json());
 citoid.use(bodyParser.urlencoded({extended: false}));
+//citoid.use(bunyan());
+citoid.use(express.static('api')); //cache api pages
 
 /*Landing page*/
+/*jshint multistr: true */
 citoid.get('/', function(req, res){
 	res.setHeader("Content-Type", "text/html");
 	res.send('<!DOCTYPE html>\
@@ -77,89 +76,90 @@ citoid.get('/', function(req, res){
 /*Endpoint for retrieving citations in JSON format from a URL*/
 citoid.post('/url', function(req, res){
 
-	var format = req.body.format,
+	var opts, parsedURL,
+		format = req.body.format,
 		requestedURL = req.body.url,
-		zoteroURLWeb = util.format(zoteroURL, 'web');
+		sessionID = "123abc";
+
+	log.info(req);
 
 	//temp backwards compatibility
 	if (!format){
-		format = 'mwDeprecated';
+	 	format = 'mwDeprecated';
 	}
 
 	res.type('application/json');
 
-	//TODO: fix for async error catching
-	try {
-		var parsedURL = urlParse.parse(requestedURL);
-		//defaults to http if no protocol specified.
-		if (!parsedURL.protocol){
-			requestedURL = 'http://'+ urlParse.format(parsedURL);
-		}
-		else {requestedURL = urlParse.format(parsedURL);}
+	parsedURL = urlParse.parse(requestedURL);
+	//defaults to http if no protocol specified.
+	if (!parsedURL.protocol){
+		requestedURL = 'http://'+ urlParse.format(parsedURL);
 	}
-	catch (e){
-		console.log(e); 
-	}
+	else {requestedURL = urlParse.format(parsedURL);}
 
-	//make things work with new format for now..
-	//format = 'mediawiki';
+	opts = {
+		zoteroURL:zoteroURL,
+		sessionID: sessionID,
+		format: format
+	};
 
-	//Request from Zotero and set response
-	zoteroRequest(zoteroURLWeb, requestedURL, testSessionID, format, function(error, response, body){
-		console.log("Request made for: " + requestedURL);
-		if (response) {
-			if (!error) {
-				//501 indicates no translator availabe
-				//this is common- can indicate shortened url
-				//or a website not specified in the translators
-				if (~[500, 501].indexOf(response.statusCode)){
-					//try again with unshortened url
-					//we don't do this initially because many sites
-					//will redirect this fcn to a log-in screen
-					unshorten(requestedURL, function(expandedURL) {
-						zoteroRequest(zoteroURLWeb, expandedURL, testSessionID, format,
-							function(error, response, body){
-							if (response){
-								//if still no translator, or translation fails,
-								//send to naive scraper
-								if (~[500, 501].indexOf(response.statusCode)){
-									scrape(requestedURL, function(body){
-										res.statusCode = 200;
-										res.json(body);
-									});
-								}
-								else {
-									res.statusCode = 200;
-									res.json(body);
-								}
-							}							
-						});
-					});
-				}
-				else {
-					res.statusCode = response.statusCode;
-					res.json(body);
-				}
-			}
-			else {
-				res.statusCode = 500;
-				res.json("Internal server error");
-				console.log(error);
-			}
+	requestFromURL(requestedURL, opts, function(error, responseCode, body){
+		if (!error){
+			res.statusCode = responseCode;
+			res.send(body);
 		}
 		else {
-			//no response
-			var message = "Server at "+zoteroURL+" does not appear to be running.";
-			res.statusCode = 500;
-			res.json("Internal server error");
-			console.log(message);
+			res.statusCode = 520;
+			res.send(body);
 		}
 	});
 });
 
+/**Endpoint for retrieving citations based on search term (URL,DOI)*/
+citoid.get('/api', function(req, res){
+	var opts, dSearch,
+		format = req.query.format,
+		search = req.query.search;
+
+	log.info(req);
+
+	if (!search){
+		res.statusCode = 400;
+		res.setHeader("Content-Type", "text/plain");
+		res.send("No 'search' value specified\n");
+	} else if(!format){
+		res.statusCode = 400;
+		res.setHeader("Content-Type", "text/plain");
+		res.send("No 'format' value specified\nOptions are 'mediawiki','zotero'");
+	} else {
+
+		dSearch = decodeURIComponent(search); //decode urlencoded search string
+
+		distinguish(dSearch, function(extractedID, runnerFunction){
+			opts = {
+				zoteroURL:zoteroURL,
+				sessionID:"123abc",
+				format:format,
+				identifier:extractedID
+			};
+
+			runnerFunction(extractedID, opts, function(error, responseCode, body){
+				if (!error){
+					res.statusCode = responseCode;
+					res.send(body);
+				}
+				else {
+					res.statusCode = 520; //Server at requested location not available
+					res.send(body);
+				}
+			});
+		});
+	}
+});
+
 citoid.listen(citoidPort);
 
-console.log('Server running on http://localhost:'+citoidPort);
+log.info('Server started on http://localhost:'+citoidPort);
 
 /*Exports*/
 exports = module.exports = citoid;

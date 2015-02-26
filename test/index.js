f59613eeb332ebac11a0832d2dcfc79d8ca5777d
Marielle Volz <marielle.volz@gmail.com>
2015-02-26 18:34:58 +0000

    Add ability to request AcceptLanguage
    *Retrieves accept-language header in both api
    and url endpoints
    
    *Uses same value of accept-language to make
    requests in lib/Scraper.js
    
    *Add test for requesting twitter.com in German
    
    *Modify existing tests to check more fields in
    citation.
    
    *Change first argument from url to opts, an
    options argument, to include acceptLanguage
    parameter, in many functions.
    
    Change-Id: I9a20ffed68c2a7337031c444ee141b08b2ffb074

--- test/index.js
+++ test/index.js
@@ -39,15 +39,27 @@ citoidService = new CitoidService(citoidConfig, log);
 
 describe('200', function() {
 
-	var url = 'example.com';
+	var opts = {
+		search : 'example.com',
+		format : 'mediawiki',
+		acceptLanguage : 'en'
+	},
+		expectedTitle = 'Example Domain';
 
 	it('should scrape info successfully', function(done) {
-		citoidService.request(url, 'mediawiki', function(error, responseCode, citation){
+		citoidService.request(opts, function(error, responseCode, citation){
 			if (error) {throw error;}
 			if (responseCode !== 200){
 				throw new Error('Not successful: Response code is' + responseCode);
 			}
 			if (!citation) {throw new Error ('Empty body');}
+			if (citation[0].title !== expectedTitle){
+				throw new Error('Expected title is: ' + expectedTitle +
+					";\nGot: " + citation[0].title);
+			}
+			if (!citation[0].itemType){
+				throw new Error('Missing itemType');
+			}
 			done();
 			// TODO: Match retrieved citation to expected citation
 		});
@@ -56,15 +68,52 @@ describe('200', function() {
 
 describe('520', function() {
 
-	var url = 'example./com';
+	var opts = {
+		search : 'example./com',
+		format : 'mediawiki',
+		acceptLanguage : 'en'
+	},
+		expectedTitle = 'http://example./com';
 
 	it('should return 520 error and citation', function(done) {
-		citoidService.request(url, 'mediawiki', function(error, responseCode, citation){
+		citoidService.request(opts, function(error, responseCode, citation){
 			if (responseCode !== 520){
 				throw new Error('Should throw 520: Response code is' + responseCode);
 			}
 			if (!citation) {throw new Error ('Empty body');}
+			if (citation[0].title !== expectedTitle){
+				throw new Error('Expected title is: ' + expectedTitle +
+					";\nGot: " + citation[0].title);
+			}
+			if (!citation[0].itemType){
+				throw new Error('Missing itemType');
+			}
 			// TODO: Match retrieved citation to expected citation
+			done();
+		});
+	});
+});
+
+describe('German twitter', function() {
+
+	var opts = {
+		search : 'http://twitter.com',
+		format : 'mediawiki',
+		acceptLanguage : 'de'
+		},
+		expectedTitle = 'Willkommen bei Twitter - Anmelden oder Registrieren';
+
+	it('should return the citation for twitter in German', function(done) {
+		citoidService.request(opts, function(error, responseCode, citation){
+			if (responseCode !== 520){
+				throw new Error('Should throw 520: Response code is' +
+					responseCode);
+			}
+			if (!citation) {throw new Error ('Empty body');}
+			if (citation[0].title !== expectedTitle){
+				throw new Error('Expected title is: ' + expectedTitle +
+					";\nGot: " + citation[0].title);
+			}
 			done();
 		});
 	});

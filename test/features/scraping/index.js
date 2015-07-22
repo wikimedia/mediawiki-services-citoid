b6c099a7adcfa9d67b9e12013639f69c770a90b7
Marko Obrovac <mobrovac@wikimedia.org>
2015-07-22 19:22:12 +0200

    Use the same cookie jar throughout a request's life
    unshorten() needs to have a cookie jar as well since some sites, such as
    ScienceDirect, do not allow the redirects to continue before seeing the
    cookie they sent. Furthermore, move the cookie jar creation to
    CitoidRequest, so that the scraper can later reuse these cookies and
    thus scrape the resource faster.
    
    This partially addresses T97042 in that it avoids redirect hell caused
    when unshorten() is trying to find the final URL to scrape.
    
    Bug: T97042 (partially only)
    Bug: T106975
    Change-Id: Id857d7eebb0d077408d6a4a4c86e3d2ab864ed5f

--- test/features/scraping/index.js
+++ test/features/scraping/index.js
@@ -135,6 +135,18 @@ describe('scraping', function() {
 			});
 		});
 
+		// Ensure DOI is present in non-zotero scraped page when request from DOI link
+		it('DOI which requires cookie to properly follow redirect to Zotero; no results from crossRef', function() {
+			return server.query('10.1642/0004-8038(2005)122[0673:PROAGP]2.0.CO;2').then(function(res) {
+				assert.status(res, 200);
+				assert.checkZotCitation(res, 'Phylogenetic relationships of antpitta genera (passeriformes: formicariidae)');
+				assert.deepEqual(res.body[0].publicationTitle, 'The Auk', 'Incorrect publicationTitle; Expected The Auk, got' + res.body[0].publicationTitle);
+				assert.deepEqual(!!res.body[0].DOI, true, 'Missing DOI');
+				assert.deepEqual(!!res.body[0].issue, true, 'Missing issue');
+				assert.deepEqual(!!res.body[0].volume, true, 'Missing volume');
+			});
+		});
+
 		it('doi pointing to bookSection', function() {
 			return server.query('10.1007/11926078_68').then(function(res) {
 				assert.status(res, 200);

f6883ea8d930b6cd7cfc07673a9ec090e29c3363
mvolz <marielle.volz@gmail.com>
2019-01-30 12:11:12 +0000

    Update tests for zotero encoding issue
    Update corresponding tests for the
    encoding issue in zotero, change
    I5d803216
    
    Bug: T212490
    Change-Id: Ifab1650fa5a3a1dd544bdc5e0a6e5f433da2877c

--- test/features/scraping/lang.js
+++ test/features/scraping/lang.js
@@ -33,6 +33,24 @@ describe('Languages: ', function() {
             });
         });
 
+        // Support for language encoding other than those native to Node
+        it('non-native to node encoding in response', function() {
+            return server.query('http://corriere.it/esteri/15_marzo_27/aereo-germanwings-indizi-interessanti-casa-copilota-ff5e34f8-d446-11e4-831f-650093316b0e.shtml').then(function(res) {
+                assert.status(res, 200);
+                assert.checkZotCitation(res, 'Aereo Germanwings, «indizi interessanti» nella casa del copilota');
+                assert.deepEqual(!!res.body[0].accessDate, true, 'No accessDate present');
+            });
+        });
+
+        // Support for language encoding other than those native to Node
+        it('content-type header present in body but not in response headers', function() {
+            return server.query('https://www.insee.fr/fr/statistiques/zones/2021173').then(function(res) {
+                assert.status(res, 200);
+                assert.checkZotCitation(res, 'Populations légales 2013 − Ces données sont disponibles sur toutes les communes de France | Insee');
+                assert.deepEqual(!!res.body[0].accessDate, true, 'No accessDate present');
+            });
+        });
+
     });
 
     describe('Using native scraper: ', function() {

93da27b96dab77b52d696ec3358b7ce226af2d0e
mvolz <marielle.volz@gmail.com>
2019-03-06 14:01:16 +0000

    Allow ISBNs with 979 prefix
    Regex in multiple places were improperly
    excluding ISBNs with a 979 prefix.
    
    Bug: T217677
    Change-Id: I257751ab47174e820fdfa75ae39da04401b0fbed

--- test/features/scraping/isbn.js
+++ test/features/scraping/isbn.js
@@ -32,7 +32,6 @@ describe('ISBN tests: ', function() {
                 assert.status(res, 200);
                 assert.checkZotCitation(res, 'MediaWiki');
                 assert.deepEqual(!!res.body[0].oclc, true, 'Missing OCLC');
-                assert.isInArray(res.body[0].source, 'Zotero');
                 assert.deepEqual(res.body[0].author, [['Daniel J.', 'Barrett']], 'Unexpected value; expected [[\'Daniel J.\'], [\'Barrett.\']] ' + res.body[0].author);
                 assert.deepEqual(res.body[0].publisher, 'O\'Reilly Media', 'Unexpected value; expected O\'Reilly Media, got ' + res.body[0].publisher);
                 assert.deepEqual(res.body[0].place, 'Sebastapool, Calif', 'Unexpected value; expected Sebastapool, Calif., got ' + res.body[0].place);
@@ -49,7 +48,6 @@ describe('ISBN tests: ', function() {
         //        assert.checkZotCitation(res, 'Harry Potter and the half-blood prince'); // No url
                 assert.deepEqual(res.body[0].title, 'Harry Potter and the Half-Blood Prince', 'Unexpected value; expected "Harry Potter and the Half-blood Prince," got ' + res.body[0].title);
         //        assert.deepEqual(!!res.body[0].oclc, true, 'Missing OCLC');
-                assert.isInArray(res.body[0].source, 'Zotero');
                 assert.deepEqual(res.body[0].author, [['J. K.', 'Rowling'], ['Mary','GrandPré']]);
                 assert.deepEqual(res.body[0].place, 'New York, NY', 'Unexpected value; expected New York, NY, got ' + res.body[0].place);
                 assert.deepEqual(res.body[0].edition, '1st American ed', 'Unexpected value; expected 1st ed., got ' + res.body[0].edition);
@@ -63,12 +61,22 @@ describe('ISBN tests: ', function() {
                 assert.status(res, 200);
                 assert.checkZotCitation(res, 'Eyewitness DVD.'); // Not great
                 assert.deepEqual(!!res.body[0].oclc, true, 'Missing OCLC');
-                assert.isInArray(res.body[0].source, 'Zotero');
         //        assert.deepEqual(!!res.body[0].author, true, 'Missing author');
                 assert.deepEqual(res.body[0].publisher, 'DK Pub.', 'Unexpected value; expected DK Pub., got ' + res.body[0].publisher);
         //        assert.deepEqual(res.body[0].place, 'New York', 'Unexpected value; expected New York, got ' + res.body[0].place);
                 assert.deepEqual(res.body[0].date, '2010', 'Unexpected value; expected 2010, got ' + res.body[0].date);
                 assert.isInArray(res.body[0].ISBN, '9780756662967');
+                assert.deepEqual(res.body[0].itemType, 'book', 'Wrong itemType; expected book, got ' + res.body[0].itemType);
+            });
+        });
+
+        it('ISBN with 979 prefix; not as good data as worldcat', function() {
+            return server.query('9791029801297').then(function(res) {
+                assert.status(res, 200);
+                assert.checkZotCitation(res, 'Mon jardin tropical: [guide de jardinage');
+                assert.deepEqual(!!res.body[0].oclc, true, 'Missing OCLC');
+//                assert.deepEqual(res.body[0].date, '2002', 'Unexpected value; expected 2002, got ' + res.body[0].date);
+                assert.isInArray(res.body[0].ISBN, '9791029801297');
                 assert.deepEqual(res.body[0].itemType, 'book', 'Wrong itemType; expected book, got ' + res.body[0].itemType);
             });
         });
@@ -93,7 +101,7 @@ describe('ISBN tests: ', function() {
             zotero: false
         }); });
 
-        it('valid book ISBN - type Text', function() {
+        it('book ISBN with dashes, type Text', function() {
             return server.query('978-0-596-51979-7').then(function(res) {
                 assert.status(res, 200);
                 assert.checkCitation(res, 'MediaWiki');
@@ -109,7 +117,20 @@ describe('ISBN tests: ', function() {
             });
         });
 
-        it('valid DVD ISBN - type Image', function() {
+        it('ISBN with 979 prefix', function() {
+            return server.query('9791029801297').then(function(res) {
+                assert.status(res, 200);
+                assert.checkCitation(res, 'Mon jardin tropical : [guide de jardinage : Antilles [et] Réunion]');
+                assert.deepEqual(!!res.body[0].oclc, true, 'Missing OCLC');
+                assert.isInArray(res.body[0].source, 'WorldCat');
+                assert.deepEqual(res.body[0].date, '2002', 'Unexpected value; expected 2002, got ' + res.body[0].date);
+                assert.isInArray(res.body[0].ISBN, '9791029801297');
+                assert.deepEqual(res.body[0].itemType, 'book', 'Wrong itemType; expected book, got ' + res.body[0].itemType);
+            });
+        });
+
+
+        it('DVD ISBN - type Image', function() {
             return server.query('978-0756662967').then(function(res) {
                 assert.status(res, 200);
                 assert.checkCitation(res); // Returns either 'Seashore' or 'Eyewitness DVD. Seashore.' as title
@@ -124,7 +145,7 @@ describe('ISBN tests: ', function() {
             });
         });
 
-        it('valid DVD ISBN - invalid Type', function() {
+        it('DVD ISBN - invalid Type', function() {
             return server.query('9780783244396').then(function(res) {
                 assert.status(res, 200);
                 assert.checkCitation(res); // Title varies: 'Jaws' or 'Jaws'

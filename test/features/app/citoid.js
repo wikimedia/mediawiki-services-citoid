d2936325189b19611bd472e7c2b1dd527020c114
Marielle Volz <marielle.volz@gmail.com>
2023-04-10 13:00:10 +0100

    Add backwards compatibility shim for restbase
    Add routing to make url based requests akin to restbase
    requests supported by the root url, such that if the app is
    served from https://{baseurl}/api/rest_v1/data/citation/
    backwards compatibility with user scripts/VE should be seamless.
    
    Adds swagger docs for restbase format.
    
    Serve ?doc at root url except when ?spec
    is requested.
    
    Increment to 1.2.0 as this adds functionality to the API.
    
    Bug: T338425
    Change-Id: I85f33c92aee2595018d4cc06e97229f4bf678868

---
+++ test/features/app/citoid.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const preq = require( 'preq' );
+const assert = require( '../../utils/assert.js' );
+const Server = require( '../../utils/server.js' );
+
+describe( 'citoid routing', function () {
+
+	this.timeout( 20000 );
+	const server = new Server();
+
+	before( () => server.start() );
+
+	after( () => server.stop() );
+
+	it( 'should get restbase style shim request for uri', () => {
+		return preq.get( {
+			uri: `${ server.config.uri }mediawiki/http%3A%2F%2Fwww.example.com`
+		} ).then( ( res ) => {
+			assert.deepEqual( res.status, 200 );
+			assert.deepEqual( res.body[ 0 ].title, 'Example Domain' );
+		} );
+	} );
+
+	it( 'should error for missing search param', () => {
+		return preq.get( {
+			uri: `${ server.config.uri }mediawiki/`
+		} ).then( ( res ) => {
+			assert.deepEqual( res.status, 400 );
+		}, function ( err ) {
+			assert.checkError( err, 400, "No 'search' value specified" );
+		} );
+	} );
+
+	it( 'should get restbase style shim request for doi', () => {
+		return preq.get( {
+			uri: `${ server.config.uri }mediawiki/10.1371%2Fjournal.pcbi.1002947`
+		} ).then( ( res ) => {
+			assert.deepEqual( res.status, 200 );
+			assert.deepEqual( res.body[ 0 ].title, 'Viral Phylodynamics' );
+		} );
+	} );
+
+	it( 'should get non-restbase style request for uri', () => {
+		return server.query( 'http://example.com' ).then( function ( res ) {
+			assert.status( res, 200 );
+			assert.deepEqual( res.body[ 0 ].title, 'Example Domain' );
+		} );
+	} );
+
+} );

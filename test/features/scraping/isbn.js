798c345d42329e58c7840d2897665b1c6adbfcd5
Marielle Volz <marielle.volz@gmail.com>
2023-06-12 10:23:20 +0100

    Remove support for worldcat search API
    WorldCat Search API is deprecated, this removes support
    for it.
    
    Note that ISBN lookup is still available via Zotero if
    citoid is being used in conjunction with Zotero.
    
    Bug: T336297
    Change-Id: I56e8bb6abc0b687d145eb6a4250d3fee9e54c7f9

--- test/features/scraping/isbn.js
+++ test/features/scraping/isbn.js
@@ -1,5 +1,5 @@
 /**
- * Tests ISBN which uses worldcat service
+ * Tests ISBN which uses zotero ISBN lookup service
  */
 
 'use strict';
@@ -13,9 +13,9 @@ describe('ISBN tests:', function () {
     const server = new Server();
 
     // Use zotero search endpoint for isbn
-    describe('zotero isbn only:', function () {
+    describe('zotero isbn:', function () {
 
-        before(() => server.start({ wskey: false }));
+        before(() => server.start());
         after(() => server.stop());
 
         it('valid ISBN', function () {
@@ -47,7 +47,7 @@ describe('ISBN tests:', function () {
             });
         });
 
-        it('valid DVD ISBN - type Image', function () {
+        it.skip('valid DVD ISBN - type Image', function () { // Requires worldcat translator
             return server.query('978-0756662967').then(function (res) {
                 assert.status(res, 200);
                 assert.checkZotCitation(res, 'Eyewitness DVD.'); // Not great
@@ -57,98 +57,6 @@ describe('ISBN tests:', function () {
                 // assert.deepEqual(res.body[0].place, 'New York', 'Unexpected value; expected New York, got ' + res.body[0].place);
                 assert.deepEqual(res.body[0].date, '2010', 'Unexpected value; expected 2010, got ' + res.body[0].date);
                 assert.isInArray(res.body[0].ISBN, '978-0-7566-6296-7');
-                assert.deepEqual(res.body[0].itemType, 'book', 'Wrong itemType; expected book, got ' + res.body[0].itemType);
-            });
-        });
-
-        it('invalid ISBN', function () {
-            const isbn = '9780596519798';
-            return server.query(isbn, 'mediawiki', 'en')
-                .then(function (res) {
-                    assert.status(res, 404);
-                }, function (err) {
-                    assert.checkError(err, 404, 'Unable to retrieve data from ISBN ' + isbn,
-                        'Unexpected error message ' + err.body.Error);
-                });
-        });
-    });
-
-    // Uses worldcat search api. This requires a working wskey in your config.yaml file. Free temporary keys available here:
-    // https://platform.worldcat.org/wskey/keys/manage
-    describe('worldcat search api only: ', function () {
-
-        before(() => server.start({ zotero: false }));
-        after(() => server.stop());
-
-        it('book ISBN with dashes, type Text', function () {
-            return server.query('978-0-596-51979-7').then(function (res) {
-                assert.status(res, 200);
-                assert.checkCitation(res, 'MediaWiki');
-                assert.deepEqual(!!res.body[0].oclc, true, 'Missing OCLC');
-                assert.isInArray(res.body[0].source, 'WorldCat');
-                assert.deepEqual(res.body[0].author, [ [ 'Daniel J.', 'Barrett' ] ], 'Unexpected value:' + res.body[0].author);
-                assert.deepEqual(res.body[0].publisher, 'O\'Reilly Media', 'Unexpected value; expected O\'Reilly Media, got ' + res.body[0].publisher);
-                assert.deepEqual(res.body[0].place, 'Sebastapool, Calif.', 'Unexpected value; expected Sebastapool, Calif., got ' + res.body[0].place); // Not currently working with Worldcat Search API - not present in results
-                assert.deepEqual(res.body[0].edition, '1st ed', 'Unexpected value; expected 1st ed., got ' + res.body[0].edition); // Not currently working with Worldcat Search API - present in description tag
-                assert.deepEqual(res.body[0].date, '2009', 'Unexpected value; expected 2009, got ' + res.body[0].date);
-                assert.isInArray(res.body[0].ISBN, '978-0-596-51979-7');
-                assert.deepEqual(res.body[0].itemType, 'book', 'Wrong itemType; expected book, got ' + res.body[0].itemType);
-            });
-        });
-
-        it('ISBN with 979 prefix', function () {
-            return server.query('9791029801297').then(function (res) {
-                assert.status(res, 200);
-                assert.checkCitation(res, 'Mon jardin tropical : [guide de jardinage : Antilles [et] Réunion]');
-                assert.deepEqual(!!res.body[0].oclc, true, 'Missing OCLC');
-                assert.isInArray(res.body[0].source, 'WorldCat');
-                assert.deepEqual(res.body[0].author, [ [ 'Aline', 'Ternisien' ] ], 'Unexpected value:' + res.body[0].author); // only get this sometimes
-                assert.deepEqual(res.body[0].contributor, [ [ 'Fabrice', 'Le Bellec' ], [ 'Éric', 'Leroy' ] ], 'Unexpected value:' + res.body[0].contributor); // only get this sometimes
-                assert.deepEqual(res.body[0].date, '2002', 'Unexpected value; expected 2002, got ' + res.body[0].date);
-                assert.isInArray(res.body[0].ISBN, '979-10-298-0129-7');
-                assert.deepEqual(res.body[0].itemType, 'book', 'Wrong itemType; expected book, got ' + res.body[0].itemType);
-            });
-        });
-
-        it('DVD ISBN - type Image', function () {
-            return server.query('978-0756662967').then(function (res) {
-                assert.status(res, 200);
-                assert.checkCitation(res, 'Seashore');
-                assert.deepEqual(!!res.body[0].oclc, true, 'Missing OCLC');
-                assert.isInArray(res.body[0].source, 'WorldCat');
-                assert.deepEqual(res.body[0].contributor, [ [ 'Martin', 'Sheen' ], [ 'Bonni', 'Cohen' ], [ 'Richard', 'Thomson' ], [ 'Inc', 'DK Publishing' ] ], 'Unexpected value:' + res.body[0].author); // only get this sometimes
-                assert.deepEqual(res.body[0].studio, 'DK Pub', 'Unexpected value; expected DK Pub, got ' + res.body[0].studio);
-                assert.deepEqual(res.body[0].place, 'New York', 'Unexpected value; expected New York, got ' + res.body[0].place);
-                assert.deepEqual(res.body[0].date, '2010', 'Unexpected value; expected 2010, got ' + res.body[0].date);
-                assert.isInArray(res.body[0].ISBN, '978-0-7566-6296-7');
-                assert.deepEqual(res.body[0].itemType, 'videoRecording', 'Wrong itemType; expected videoRecording, got ' + res.body[0].itemType);
-            });
-        });
-
-        it('DVD ISBN - many types in type list', function () {
-            return server.query('9780783244396').then(function (res) {
-                assert.status(res, 200);
-                assert.checkCitation(res, 'Jaws');
-                assert.deepEqual(!!res.body[0].oclc, true, 'Missing OCLC');
-                assert.isInArray(res.body[0].source, 'WorldCat');
-                assert.deepEqual(!!res.body[0].contributor, true, 'Missing contributor');
-                assert.deepEqual(res.body[0].studio, 'Universal Pictures (Firm),', 'Unexpected value; expected Universal Pictures (Firm),, got ' + res.body[0].studio);
-                assert.deepEqual(res.body[0].date, '2000', 'Unexpected value; expected 2000, got ' + res.body[0].date);
-                assert.isInArray(res.body[0].ISBN, '978-0-7832-4439-6');
-                assert.deepEqual(res.body[0].itemType, 'videoRecording', 'Wrong itemType; expected videoRecording, got ' + res.body[0].itemType);
-            });
-        });
-
-        it('valid ISBN with funky author field', function () {
-            return server.query('9780439784542').then(function (res) {
-                assert.status(res, 200);
-                assert.checkCitation(res, 'Harry Potter and the Half-Blood Prince');
-                assert.deepEqual(!!res.body[0].oclc, true, 'Missing OCLC');
-                assert.isInArray(res.body[0].source, 'WorldCat');
-                assert.deepEqual(res.body[0].author, [ [ 'J. K.', 'Rowling' ] ], 'Unexpected value:' + res.body[0].author);
-                assert.deepEqual(res.body[0].place, 'New York, NY', 'Unexpected value; expected New York, NY, got ' + res.body[0].place);
-                assert.deepEqual(res.body[0].edition, 'First American edition', 'Unexpected value; expected First American edition, got ' + res.body[0].edition);
-                assert.isInArray(res.body[0].ISBN, '978-0-439-78454-2');
                 assert.deepEqual(res.body[0].itemType, 'book', 'Wrong itemType; expected book, got ' + res.body[0].itemType);
             });
         });
--- test/features/scraping/search.js
+++ test/features/scraping/search.js
@@ -3,13 +3,13 @@
 const assert = require('../../utils/assert.js');
 const Server = require('../../utils/server.js');
 
-describe('Search results where we expect multiple results', function () {
+describe('Freetext or ambiguous search, classified as "any" input type, i.e. title or citation', function () {
 
-    describe('Default config (worldcat disabled)', function () {
+    describe('Default config', function () {
 
         this.timeout(20000);
         const server = new Server();
-        before(() => server.start({ wskey: false }));
+        before(() => server.start());
         after(() => server.stop());
 
         // Previously gave error; now passes to search
@@ -50,21 +50,21 @@ describe('Search results where we expect multiple results', function () {
         it('Open search for Schrodinger', function () {
             return server.query('E. Schrodinger, Proc. Cam. Phil. Soc. 31, 555 (1935)').then(function (res) {
                 assert.checkCitation(res, 'Discussion of Probability Relations between Separated Systems');
-                assert.deepEqual(res.body.length, 1); // One from Crossref; Empty results from worldcat as disabled
+                assert.deepEqual(res.body.length, 1); // One from Crossref
             });
         });
 
         it('Open search containing <> works; but gets wrong results from crossRef', function () {
             return server.query('Title. Available at: <http://www.example.com>. Accessed on May 19, 1998.').then(function (res) {
                 assert.checkCitation(res);
-                assert.deepEqual(res.body.length, 2); // One from url, one from Crossref; Empty results from Worldcat as disabled
+                assert.deepEqual(res.body.length, 2); // One from url, one from Crossref
             });
         });
 
         it('Open search with www but no protocol', function () {
             return server.query('Title. Available at: <www.example.com>. Accessed on May 19, 1998.').then(function (res) {
                 assert.status(res, 200);
-                assert.deepEqual(res.body.length, 2); // One from url, one from Crossref; Empty results from Worldcat as disabled.
+                assert.deepEqual(res.body.length, 2); // One from url, one from Crossref
             });
         });
 
@@ -101,120 +101,4 @@ describe('Search results where we expect multiple results', function () {
         });
     });
 
-    // WSKEY required from worldcat to run these tests
-    describe('worldcat enabled', function () {
-
-        this.timeout(20000);
-        const server = new Server();
-        before(() => server.start());
-        after(() => server.stop());
-
-        describe('mediawiki format', function () {
-            // Previously gave error; now passes to search
-            it('spaces in url missing http:// and www', function () {
-                const url = 'example.com/spaces in url';
-                return server.query(url, 'mediawiki', 'en')
-                    .then(function (res) {
-                        assert.status(res, 200);
-                    });
-            });
-
-            // Uses json as plain text search term; previously gave error
-            it('json in search', function () {
-                return server.query('{"json":"object"}', 'mediawiki', 'en')
-                    .then(function (res) {
-                        assert.status(res, 200);
-                    });
-            });
-
-            // Uses search; previously gave error
-            it('javascript in search', function () {
-                return server.query('f<script>alert(1);</script>', 'mediawiki', 'en')
-                    .then(function (res) {
-                        assert.status(res, 200);
-                    });
-            });
-
-            // Uses search; previously gave error
-            it('localhost:1970', function () {
-                return server.query('localhost:1970', 'mediawiki', 'en')
-                    .then(function (res) {
-                        assert.status(res, 200);
-                    }, function (err, res) {
-                        assert.status(err, 200);
-                    });
-            });
-
-            it('Open search for Schrodinger', function () {
-                return server.query('E. Schrodinger, Proc. Cam. Phil. Soc. 31, 555 (1935)').then(function (res) {
-                    assert.checkCitation(res, 'Discussion of Probability Relations between Separated Systems');
-                    assert.deepEqual(res.body.length, 1); // One from Crossref; Empty results from worldcat for some reason
-                });
-            });
-
-            it('Open search containing <> works; but gets wrong results from crossRef', function () {
-                return server.query('Title. Available at: <http://www.example.com>. Accessed on May 19, 1998.').then(function (res) {
-                    assert.checkCitation(res);
-                    assert.deepEqual(res.body.length, 2); // One from url, one from Crossref; Empty results from Worldcat for some reason
-                });
-            });
-
-            it('Open search with www but no protocol', function () {
-                return server.query('Title. Available at: <www.example.com>. Accessed on May 19, 1998.').then(function (res) {
-                    assert.checkCitation(res);
-                    assert.deepEqual(res.body.length, 2); // One from Crossref; Empty results from Worldcat for some reason
-                });
-            });
-
-            it('Open search with doi', function () {
-                return server.query('Kingsolver JG, Hoekstra HE, Hoekstra JM, Berrigan D, Vignieri SN, Hill CE, Hoang A, Gibert P, Beerli P (2001) Data from: The strength of phenotypic selection in natural populations. Dryad Digital Repository. doi:10.5061/dryad.166').then(function (res) {
-                    assert.checkZotCitation(res, 'Data from: The strength of phenotypic selection in natural populations');
-                    assert.deepEqual(res.body.length, 1); // One citation from detected DOI
-                });
-            });
-
-            // Gets correct data from url, incorrect data from crossRef
-            it('Open search with url', function () {
-                return server.query('Frederico Girosi; Gary King, 2006, ‘Cause of Death Data’, http://hdl.handle.net/1902.1/UOVMCPSWOL UNF:3:9JU+SmVyHgwRhAKclQ85Cg== IQSS Dataverse Network [Distributor] V3 [Version].').then(function (res) {
-                    assert.checkCitation(res);
-                    assert.deepEqual(res.body.length, 2); // One from Worldcat, one from Crossref
-                });
-            });
-
-            // Gets item from single search term
-            it('Open search with single term', function () {
-                return server.query('Mediawiki').then(function (res) {
-                    assert.checkCitation(res);
-                    assert.deepEqual(res.body.length, 2); // One from Worldcat, one from Crossref
-                });
-            });
-
-            // Gets item from single search term
-            it('Harry Potter', function () {
-                return server.query('Mediawiki').then(function (res) {
-                    assert.checkCitation(res);
-                    assert.deepEqual(res.body.length, 2); // One from Worldcat, one from Crossref
-                });
-            });
-
-            it('non-isbn in dc:identifiers', function () {
-                return server.query('Istorijsko poreklo Srba').then(function (res) {
-                    assert.status(res, 200);
-                    assert.checkCitation(res);
-                    assert.isInArray(res.body[1].ISBN, '978-86-911149-2-3');
-                    assert.isNotInArray(res.body[1].ISBN, '2012467583');
-                    assert.deepEqual(res.body[1].itemType, 'book', 'Wrong itemType; expected book, got ' + res.body[0].itemType);
-                });
-            });
-        });
-
-        describe('wikibase format', function () {
-            // Gets item from single search term
-            it('Harry Potter', function () {
-                return server.query('Harry Potter', 'wikibase').then(function (res) {
-                    assert.deepEqual(res.body.length, 1); // One from Crossref - worldcat disabled for wikibase
-                });
-            });
-        });
-    });
 });

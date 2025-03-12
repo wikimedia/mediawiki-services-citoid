12546ef03b9ad751cded995b3fdbcdb0c3883cfd
Marielle Volz <mvolz@wikimedia.org>
2025-03-12 13:17:09 +0000

    Regression: Fix TypeError
    This throws a type error in some places because it returns
    the object, not a promise.
    
    Return a promise for the object instead.
    
    Add unit test to prevent future regressions.
    
    Change-Id: I3eba7b858c13d03db64511a4f0c4d9cb9fd77226

---
+++ test/features/unit/unshorten.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const fs = require( 'fs' );
+const yaml = require( 'js-yaml' );
+
+const assert = require( '../../utils/assert.js' );
+const unshorten = require( '../../.././lib/unshorten.js' );
+
+const CitoidRequest = require( '../../.././lib/CitoidRequest.js' );
+
+describe( 'lib/unshorten.js', () => {
+
+	let result;
+	let url;
+	let app;
+	let conf;
+	let cr;
+	let citation;
+
+	before( () => {
+		conf = yaml.safeLoad( fs.readFileSync( __dirname + '/../../../config.yaml' ) );
+		app = {
+			conf: conf.services[ 0 ].conf,
+			citoid: { exporter: {} } // Dummy exporter
+		};
+		cr = new CitoidRequest( { query: { format: 'mediawiki' }, headers: {}, logger: console }, app );
+	} );
+
+	it( 'Returns successful Promise if already unshortened', () => {
+		url = 'http://www.example.com';
+		citation = { resolvedUrl: url };
+		result = unshorten( url, cr, citation );
+		assert.ok( result.then ); // Check if has then method to detect Promise
+	} );
+
+} );

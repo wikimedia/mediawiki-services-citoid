df7f964f2f45bca0b40f6bed3a6cfa13fa53414e
Marielle Volz <mvolz@wikimedia.org>
2025-03-20 11:51:30 +0000

    Split basefields tests
    Put basefields-related tests in zotero folder
    where such tests require it.
    
    Change-Id: Ic31cd619eb1d9ca17d22fd0f8428401b15231b7c

---
+++ test/zotero/scraping/basefields.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const assert = require( '../../utils/assert.js' );
+const Server = require( '../../utils/server.js' );
+
+describe( 'correctly gets base fields instead of more specific fields (using zotero)', function () {
+
+	this.timeout( 20000 );
+	const server = new Server();
+
+	before( () => server.start( { zotero: true } ) );
+
+	it( 'conferencePaper', () => server.query( '10.1007/11926078_68', 'mediawiki-basefields', 'en' ).then( ( res ) => {
+		assert.status( res, 200 );
+		assert.isInArray( res.body[ 0 ].source, 'Zotero', 'Expected response from Zotero' );
+		assert.deepEqual( !!res.body[ 0 ].publicationTitle, true, 'Missing publicationTitle field' );
+		assert.deepEqual( !!res.body[ 0 ].proceedingsTitle, false, 'Missing proceedingsTitle field' );
+	} ) );
+
+	it( 'encyclopediaArticle', () => server.query( 'http://fr.wikipedia.org/w/index.php?title=Ninja_Turtles_(film)&oldid=115125238',
+		'mediawiki-basefields', 'en', 'true' ).then( ( res ) => {
+		assert.status( res, 200 );
+		assert.isInArray( res.body[ 0 ].source, 'Zotero', 'Expected response from Zotero' );
+		assert.deepEqual( !!res.body[ 0 ].publicationTitle, true, 'Missing publicationTitle field' );
+		assert.deepEqual( !!res.body[ 0 ].encyclopediaTitle, false, 'Missing encyclopediaTitle field' );
+	} ) );
+
+	after( () => server.stop() );
+	// TODO: Add test for creator field basefields
+
+} );

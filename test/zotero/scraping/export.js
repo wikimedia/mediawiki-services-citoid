d73ad7fa13ac9034484b355cc76aa4ce3e133f06
Marielle Volz <marielle.volz@gmail.com>
2025-08-26 17:06:43 +0100

    Redo "Remove preq from ZoteroService"
    Redo of I4ce142e4
    
    * Error handling differed from preq and this *may*
    have have resulted in the increase in 500 internal server errors.
    Attempt to resolve this by throwing HTTP errors and wrapping
    in a bluebird promise.
    
    * Add tests to ensure errors are handled correctly
    when exporting into a different format. Unfortunately
    these tests don't manage to replicate the production 500s, but
    we're now logging details of 500s (done in  I4a576fa4528c7)
    so if this *doesn't* fix the issue,
    it'll be easier to debug. 
    
    Bug: T361602
    Change-Id: Id293804f1e2d320b17c1b0ca57a67520e750bcec

--- test/zotero/scraping/export.js
+++ test/zotero/scraping/export.js
@@ -3,6 +3,10 @@
 const assert = require( '../../utils/assert.js' );
 const Server = require( '../../utils/server.js' );
 
+const url = 'https://en.wikipedia.org/404';
+const doi = '10.1000/thisdoidoesntexist';
+const localhost = 'http://localhost:1970';
+
 describe( 'Exports into non mediawiki formats:', () => {
 
 	const server = new Server();
@@ -27,6 +31,25 @@ describe( 'Exports into non mediawiki formats:', () => {
 			assert.checkBibtex( res, '\n@article{chobanian_seventh_20' );
 		} ) );
 
+		it( '404', () => server.query( url, 'bibtex' ).then( ( res ) => {
+			assert.status( res, 404 );
+		}, ( err ) => {
+			assert.status( err, 404 );
+		} ) );
+
+		it( 'invalid DOI', () => server.query( doi, 'bibtex' ).then( ( res ) => {
+			assert.status( res, 404 );
+		}, ( err ) => {
+			assert.status( err, 404 );
+		} ) );
+
+		it( 'invalid address', () => server.query( localhost, 'bibtex', 'en' )
+			.then( ( res ) => {
+				assert.status( res, 400 );
+			}, ( err, res ) => {
+				assert.status( err, 400 );
+			} ) );
+
 	} );
 
 	describe( 'Exporting to zotero: ', () => {
@@ -46,20 +69,37 @@ describe( 'Exports into non mediawiki formats:', () => {
 			assert.ok( res.body[ 0 ].creators );
 			assert.ok( res.body[ 0 ].DOI );
 			assert.deepEqual( res.body[ 0 ].ISSN, '1463-9076, 1463-9084' );
-			assert.deepEqual( res.body[ 0 ].itemType, 'journalArticle', 'Wrong itemType; expected journalArticle, got' + res.body[ 0 ].itemType );
+			assert.deepEqual( res.body[ 0 ].itemType, 'journalArticle' );
 		} ) );
+
+		it( '404', () => server.query( url, 'zotero' ).then( ( res ) => {
+			assert.status( res, 404 );
+		}, ( err ) => {
+			assert.status( err, 404 );
+		} ) );
+
+		it( 'invalid DOI ', () => server.query( doi, 'zotero' ).then( ( res ) => {
+			assert.status( res, 404 );
+		}, ( err ) => {
+			assert.status( err, 404 );
+		} ) );
+
+		it( 'invalid address', () => server.query( localhost, 'zotero', 'en' )
+			.then( ( res ) => {
+				assert.status( res, 400 );
+			}, ( err, res ) => {
+				assert.status( err, 400 );
+			} ) );
 	} );
 
 	describe( 'Exporting to wikibase:', () => {
 		it( 'valid ISBN', () => server.query( '978-0-596-51979-7', 'wikibase' ).then( ( res ) => {
 			assert.status( res, 200 );
+			assert.deepEqual( res.body[ 0 ].title, 'MediaWiki' );
 			assert.deepEqual( !!res.body[ 0 ].oclc, false, 'Unexpected oclc ' + res.body[ 0 ].oclc );
-			assert.deepEqual( res.body[ 0 ].publisher, 'O\'Reilly', 'Unexpected value; expected O\'Reilly Media, got ' + res.body[ 0 ].publisher );
-			assert.deepEqual( res.body[ 0 ].place, 'Sebastapool, Calif', 'Unexpected value; expected Sebastapool, Calif., got ' + res.body[ 0 ].place );
-			assert.deepEqual( res.body[ 0 ].edition, '1st ed', 'Unexpected value; expected 1st ed., got ' + res.body[ 0 ].edition );
-			assert.deepEqual( res.body[ 0 ].date, '2009', 'Unexpected value; expected 2009, got ' + res.body[ 0 ].date );
+			assert.deepEqual( res.body[ 0 ].date, '2009' );
 			assert.isInArray( res.body[ 0 ].identifiers.isbn13, '978-0-596-51979-7' );
-			assert.deepEqual( res.body[ 0 ].itemType, 'book', 'Wrong itemType; expected book, got ' + res.body[ 0 ].itemType );
+			assert.deepEqual( res.body[ 0 ].itemType, 'book' );
 		} ) );
 
 		it( 'doi with ISSN', () => server.query( 'doi:10.1039/b309952k', 'wikibase' ).then( ( res ) => {
@@ -69,16 +109,27 @@ describe( 'Exports into non mediawiki formats:', () => {
 			assert.ok( res.body[ 0 ].creators );
 			assert.ok( res.body[ 0 ].identifiers.doi );
 			assert.deepEqual( res.body[ 0 ].ISSN, [ '1463-9076', '1463-9084' ] );
-			assert.deepEqual( res.body[ 0 ].itemType, 'journalArticle', 'Wrong itemType; expected journalArticle, got' + res.body[ 0 ].itemType );
+			assert.deepEqual( res.body[ 0 ].itemType, 'journalArticle' );
 		} ) );
-	} );
 
-	describe( 'Exporting to mwDeprecated no longer functioning : ', () => {
-		it( 'Uses formerly correct parameter', () => server.query( '10.1007/11926078_68', 'mwDeprecated' ).then( ( res ) => {
-			assert.status( res, 400 );
+		it( '404', () => server.query( url, 'bibtex' ).then( ( res ) => {
+			assert.status( res, 404 );
 		}, ( err ) => {
-			assert.checkError( err, 400, 'Invalid format requested mwDeprecated' );
+			assert.status( err, 404 );
 		} ) );
 
+		it( 'invalid DOI', () => server.query( doi, 'wikibase' ).then( ( res ) => {
+			assert.status( res, 404 );
+		}, ( err ) => {
+			assert.status( err, 404 );
+		} ) );
+
+		it( 'invalid address', () => server.query( localhost, 'wikibase', 'en' )
+			.then( ( res ) => {
+				assert.status( res, 400 );
+			}, ( err, res ) => {
+				assert.status( err, 400 );
+			} ) );
 	} );
+
 } );

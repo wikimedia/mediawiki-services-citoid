161c1223d992db698c3d094e5f13e0e4c3798ecb
Marielle Volz <marielle.volz@gmail.com>
2025-09-29 11:19:31 +0100

    Correctly handle relative redirects
    Relative redirects weren't being handled correctly,
    which would sometimes throw TypeErrors which caused
    a 500 internal server error.
    
    * Fixed regex which wouldn't match redirects to
    files in the same directory i.e. 'page.html'
    Live example:
    https://ncarchitects.lib.ncsu.edu/people/P000190
    
    * Use href instead of origin, which meant that
    redirects in the same directory wouldn't work
    because part of the full path was missing
    
    * Add mocked tests for relative redirects to ensure
    all kinds of relative redirects work.
    
    Bug: T405464
    Change-Id: I48088b056ffc9e828eb8d75d06eec59f204c327f

--- test/features/errors/redirect.js
+++ test/features/errors/redirect.js
@@ -12,86 +12,201 @@ describe( 'redirects', () => {
 
 	after( () => server.stop() );
 
-	// httpbin no longer live, so just mock its behaviour since all it does here is redirect anyway.
-	const redirector = () => {
-		const base = 'https://httpbin.org';
-		nock( base )
-			.head( '/redirect-to' )
-			.query( true )
-			.reply( ( uri ) => {
-				redirector(); // call again to enable the recursive behaviour below
-				const parsed = new URL( uri, base );
-				return [ 302, undefined, { Location: parsed.searchParams.get( 'url' ) } ];
-			} );
-	};
+	describe( 'redirect chains', () => {
 
-	beforeEach( () => {
-		redirector();
-	} );
+		// httpbin no longer live, so just mock its behaviour since all it does here is redirect anyway.
+		const redirector = () => {
+			const base = 'https://httpbin.org';
+			nock( base )
+				.head( '/redirect-to' )
+				.query( true )
+				.reply( ( uri ) => {
+					redirector(); // call again to enable the recursive behaviour below
+					const parsed = new URL( uri, base );
+					return [ 302, undefined, { Location: parsed.searchParams.get( 'url' ) } ];
+				} );
+		};
 
-	afterEach( () => {
-		nock.cleanAll();
-	} );
+		before( () => {
+			redirector();
+		} );
 
-	it( 'redirect supported', () => server.query( 'https://httpbin.org/redirect-to?url=http://www.example.com', 'mediawiki', 'en' )
-		.then( ( res ) => {
-			assert.status( res, 200 );
-		} ) );
+		after( () => {
+			nock.cleanAll();
+		} );
 
-	it( 'redir-to-private', () => server.query( 'https://httpbin.org/redirect-to?url=http://192.168.1.2', 'mediawiki', 'en' )
-		.then( ( res ) => {
-			assert.status( res, 400 );
-		}, ( err ) => {
-			assert.status( err, 400 );
-		} ) );
+		it( 'redirect supported', () => server.query( 'https://httpbin.org/redirect-to?url=http://www.example.com', 'mediawiki', 'en' )
+			.then( ( res ) => {
+				assert.status( res, 200 );
+			} ) );
 
-	it( 'redir-to-redir-private', () => server.query( 'https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=http://192.168.1.2', 'mediawiki', 'en' )
-		.then( ( res ) => {
-			assert.status( res, 400 );
-		}, ( err ) => {
-			assert.status( err, 400 );
-		} ) );
+		it( 'redir-to-private', () => server.query( 'https://httpbin.org/redirect-to?url=http://192.168.1.2', 'mediawiki', 'en' )
+			.then( ( res ) => {
+				assert.status( res, 400 );
+			}, ( err ) => {
+				assert.status( err, 400 );
+			} ) );
 
-	it( 'follows relative redirects', () => server.query( 'https://httpbin.org/redirect-to?url=/redirect-to?url=http://example.com', 'mediawiki', 'en' )
-		.then( ( res ) => {
-			assert.status( res, 200 );
-		}, ( err ) => {
-			assert.status( err, 200 );
-		} ) );
+		it( 'redir-to-redir-private', () => server.query( 'https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=http://192.168.1.2', 'mediawiki', 'en' )
+			.then( ( res ) => {
+				assert.status( res, 400 );
+			}, ( err ) => {
+				assert.status( err, 400 );
+			} ) );
 
-	it( 'redir-to-redir-to-redir-to-private', () => server.query( 'https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=http://192.168.1.2', 'mediawiki', 'en' )
-		.then( ( res ) => {
-			assert.status( res, 400 );
-		}, ( err ) => {
-			assert.status( err, 400 );
-		} ) );
-
-	it( 'five-redirect-max-by-default-under', () => {
-		const url = 'https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://en.wikipedia.org/wiki/Zotero';
-		return server.query( url, 'mediawiki', 'en' )
+		it( 'follows relative redirects', () => server.query( 'https://httpbin.org/redirect-to?url=/redirect-to?url=http://example.com', 'mediawiki', 'en' )
 			.then( ( res ) => {
 				assert.status( res, 200 );
 			}, ( err ) => {
 				assert.status( err, 200 );
-			} );
-	} );
+			} ) );
 
-	it( 'five-redirect-max-by-default-equal', () => {
-		const url = 'https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://en.wikipedia.org/wiki/Zotero';
-		return server.query( url, 'mediawiki', 'en' )
+		it( 'redir-to-redir-to-redir-to-private', () => server.query( 'https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=http://192.168.1.2', 'mediawiki', 'en' )
 			.then( ( res ) => {
-				assert.status( res, 200 );
+				assert.status( res, 400 );
 			}, ( err ) => {
-				assert.status( err, 200 );
-				assert.deepEqual( err.body.error, 'Unable to load URL ' + url );
-			} );
+				assert.status( err, 400 );
+			} ) );
+
+		it( 'five-redirect-max-by-default-under', () => {
+			const url = 'https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://en.wikipedia.org/wiki/Zotero';
+			return server.query( url, 'mediawiki', 'en' )
+				.then( ( res ) => {
+					assert.status( res, 200 );
+				}, ( err ) => {
+					assert.status( err, 200 );
+				} );
+		} );
+
+		it( 'five-redirect-max-by-default-equal', () => {
+			const url = 'https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://en.wikipedia.org/wiki/Zotero';
+			return server.query( url, 'mediawiki', 'en' )
+				.then( ( res ) => {
+					assert.status( res, 200 );
+				}, ( err ) => {
+					assert.status( err, 200 );
+					assert.deepEqual( err.body.error, 'Unable to load URL ' + url );
+				} );
+		} );
+
+		it( 'five-redirect-max-by-default-over', () => server.query( 'https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://en.wikipedia.org/wiki/Zotero', 'mediawiki', 'en' )
+			.then( ( res ) => {
+				assert.status( res, 400 );
+			}, ( err ) => {
+				assert.status( err, 400 );
+			} ) );
+
 	} );
 
-	it( 'five-redirect-max-by-default-over', () => server.query( 'https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://httpbin.org/redirect-to?url=https://en.wikipedia.org/wiki/Zotero', 'mediawiki', 'en' )
-		.then( ( res ) => {
-			assert.status( res, 400 );
-		}, ( err ) => {
-			assert.status( err, 400 );
-		} ) );
+	describe( 'relative redirects', () => {
+
+		const base = 'https://example.com';
+
+		afterEach( () => {
+			nock.cleanAll();
+		} );
+
+		it( 'Handles relative filename redirects correctly', () => {
+			nock( base )
+				.head( '/page' )
+				.reply( 302, undefined, { Location: 'page.html' } )
+				.head( '/page.html' )
+				.reply( 200 )
+				.get( '/page.html' )
+				.reply( 200, '<html><head><title>Test Page</title></head></html>' );
+
+			return server.query( 'https://example.com/page', 'mediawiki', 'en' ).then( ( res ) => {
+				assert.checkCitation( res, 'Test Page' );
+			} );
+		} );
+
+		it( 'Handles ./ relative redirects correctly', () => {
+			nock( base )
+				.head( '/dir/page' )
+				.reply( 302, undefined, { Location: './page.html' } )
+				.head( '/dir/page.html' )
+				.reply( 200 )
+				.get( '/dir/page.html' )
+				.reply( 200, '<html><head><title>Same Dir</title></head></html>' );
+
+			return server.query( 'https://example.com/dir/page', 'mediawiki', 'en' ).then( ( res ) => {
+				assert.checkCitation( res, 'Same Dir' );
+			} );
+		} );
+
+		it( 'Handles ../ parent directory redirects correctly', () => {
+			nock( base )
+				.head( '/dir/subdir/page' )
+				.reply( 302, undefined, { Location: '../page.html' } )
+				.head( '/dir/page.html' )
+				.reply( 200 )
+				.get( '/dir/page.html' )
+				.reply( 200, '<html><head><title>Parent Dir</title></head></html>' );
+
+			return server.query( 'https://example.com/dir/subdir/page', 'mediawiki', 'en' ).then( ( res ) => {
+				assert.checkCitation( res, 'Parent Dir' );
+			} );
+		} );
+
+		it( 'Handles capital letters in protocol', () => {
+			const base = 'HTTPS://example.com';
+			nock( base )
+				.head( '/dir/subdir/page' )
+				.reply( 302, undefined, { Location: '../page.html' } )
+				.head( '/dir/page.html' )
+				.reply( 200 )
+				.get( '/dir/page.html' )
+				.reply( 200, '<html><head><title>Parent Dir</title></head></html>' );
+
+			return server.query( 'https://example.com/dir/subdir/page', 'mediawiki', 'en' ).then( ( res ) => {
+				assert.checkCitation( res, 'Parent Dir' );
+			} );
+		} );
+
+		it( 'Handles subdirectory relative redirects correctly', () => {
+			nock( base )
+				.head( '/dir/page' )
+				.reply( 302, undefined, { Location: 'subdir/page.html' } )
+				.head( '/dir/subdir/page.html' )
+				.reply( 200 )
+				.get( '/dir/subdir/page.html' )
+				.reply( 200, '<html><head><title>Subdir</title></head></html>' );
+
+			return server.query( 'https://example.com/dir/page', 'mediawiki', 'en' ).then( ( res ) => {
+				assert.checkCitation( res, 'Subdir' );
+			} );
+		} );
+
+		it( 'Handles absolute path redirects correctly', () => {
+			nock( base )
+				.head( '/dir/page' )
+				.reply( 302, undefined, { Location: '/absolute/path.html' } )
+				.head( '/absolute/path.html' )
+				.reply( 200 )
+				.get( '/absolute/path.html' )
+				.reply( 200, '<html><head><title>Absolute Path</title></head></html>' );
+
+			return server.query( 'https://example.com/dir/page', 'mediawiki', 'en' ).then( ( res ) => {
+				assert.checkCitation( res, 'Absolute Path' );
+			} );
+		} );
+
+		it( 'Handles protocol-relative redirects correctly', () => {
+			nock( base )
+				.head( '/page' )
+				.reply( 302, undefined, { Location: '//en.wikipedia.com/wiki/Test' } );
+
+			nock( 'https://en.wikipedia.com' )
+				.head( '/wiki/Test' )
+				.reply( 200 )
+				.get( '/wiki/Test' )
+				.reply( 200, '<html><head><title>Protocol Relative</title></head></html>' );
+
+			return server.query( 'https://example.com/page', 'mediawiki', 'en' ).then( ( res ) => {
+				assert.checkCitation( res, 'Protocol Relative' );
+			} );
+		} );
+
+	} );
 
 } );
